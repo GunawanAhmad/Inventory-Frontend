3286252411d4612842dc0307b83732899a2a2774
Gunawan Nur Ahmad <54525132+Gunawanart@users.noreply.github.com>
2021-02-10 21:37:14 +0700

    add login page

--- src/View/login.jsx
+++ src/View/login.jsx
@@ -1,9 +1,42 @@
 import React from "react";
+import "../css/login.css";
 
 function Login() {
   return (
     <div className="container">
-      <h1 className="Title">Login page</h1>
+      <form action="" className="form">
+        <h3 className="title">LOGIN</h3>
+        <div className="form_div">
+          <input
+            type="text"
+            name="username"
+            id="username"
+            required
+            className="input"
+          />
+          <label htmlFor="username" className="label">
+            Username
+          </label>
+        </div>
+        <div className="form_div">
+          <input
+            type="password"
+            name="username"
+            id="password"
+            required
+            className="input"
+          />
+          <label htmlFor="password" className="label">
+            Password
+          </label>
+        </div>
+        <label class="checkbox-container">
+          Remember me
+          <input type="checkbox" />
+          <span class="fake-checkbox"></span>
+        </label>
+        <button className="btn">LOGIN</button>
+      </form>
     </div>
   );
 }
