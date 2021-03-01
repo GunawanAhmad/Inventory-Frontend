e02a8208f2eea5a07f0d56c05a408cfe03eb43ab
Gunawan Nur Ahmad <54525132+Gunawanart@users.noreply.github.com>
2021-03-01 17:24:11 +0700

    add list and header on beranda page

--- src/View/beranda.jsx
+++ src/View/beranda.jsx
@@ -1,12 +1,70 @@
 import React from "react";
 import "../css/beranda.css";
+import img from "../test.jpg";
 
 function Login() {
   const [milik, setMilik] = React.useState("Internal");
+  const [status, setStatus] = React.useState("Semua");
   const selectMilik = React.createRef();
-  function toggleDropdown(e) {
+  const selectStatus = React.createRef();
+
+  const listBarang = [
+    {
+      nama: "kursi",
+      kondisi: "Baik",
+      lokasi: "Gudang",
+      status: "Ada",
+      tanggalMasuk: "11/12/2020",
+      jumlah: 1,
+      satuan: "Buah",
+    },
+    {
+      nama: "kursi Duduk",
+      kondisi: "Baik",
+      lokasi: "Gudang",
+      status: "Ada",
+      tanggalMasuk: "11/12/2020",
+      jumlah: 1,
+      satuan: "Buah",
+    },
+    {
+      nama: "Meja",
+      kondisi: "Baik",
+      lokasi: "Gudang",
+      status: "Rusak",
+      tanggalMasuk: "11/12/2020",
+      jumlah: 1,
+      satuan: "Buah",
+    },
+    {
+      nama: "Lemari",
+      kondisi: "Baik",
+      lokasi: "Gudang",
+      status: "Hilang",
+      tanggalMasuk: "11/12/2020",
+      jumlah: 1,
+      satuan: "Buah",
+    },
+    {
+      nama: "Lemari",
+      kondisi: "Baik",
+      lokasi: "Gudang",
+      status: "Hilang",
+      tanggalMasuk: "11/12/2020",
+      jumlah: 1,
+      satuan: "Buah",
+    },
+  ];
+
+  function toggleMilikDropdown(e) {
     if (e.target.classList.contains("select-milik")) {
       selectMilik.current.classList.toggle("hide");
+    }
+  }
+
+  function toggleStatusDropdown(e) {
+    if (e.target.classList.contains("select-status")) {
+      selectStatus.current.classList.toggle("hide");
     }
   }
 
@@ -15,13 +73,18 @@ function Login() {
     selectMilik.current.classList.toggle("hide");
   }
 
+  function changeStatusList(val) {
+    setStatus(val);
+    selectStatus.current.classList.toggle("hide");
+  }
+
   return (
     <div className="beranda-container">
-      <div className="header-section">
+      <section className="header-section">
         <h1 className="title">Inventory</h1>
         <div
-          className="select-milik"
-          onClick={toggleDropdown}
+          className="select-milik hide"
+          onClick={toggleMilikDropdown}
           ref={selectMilik}
         >
           <span>Milik : </span>
@@ -47,7 +110,54 @@ function Login() {
 
           <input type="text" placeholder="search..." />
         </div>
-      </div>
+      </section>
+      <section
+        className="select-status hide"
+        onClick={toggleStatusDropdown}
+        ref={selectStatus}
+      >
+        <p>
+          Status : {status} <span className="triangle"></span>
+        </p>
+        <ul className="list hide">
+          <li onClick={() => changeStatusList("All")}>Semua</li>
+          <li onClick={() => changeStatusList("Ada")}>Ada</li>
+          <li onClick={() => changeStatusList("Dipinjam")}>Dipinjam</li>
+          <li onClick={() => changeStatusList("Hilang")}>Hilang</li>
+        </ul>
+      </section>
+      <section className="list-section">
+        <table className="table-container">
+          <tr className="table-head">
+            <th></th>
+            <th>Nama barang</th>
+            <th>Kondisi</th>
+            <th>Lokasi</th>
+            <th>Status</th>
+            <th>Tanggal masuk</th>
+            <th>Jumlah</th>
+          </tr>
+          {listBarang
+            .filter((barang) =>
+              status == "Semua" ? barang.status != "" : barang.status == status
+            )
+            .map((filteredList) => (
+              <tr className="item">
+                <td>
+                  <img src={img} alt="img" />
+                </td>
+                <td>{filteredList.nama}</td>
+                <td>{filteredList.kondisi}</td>
+                <td>{filteredList.lokasi}</td>
+                <td>{filteredList.status}</td>
+                <td> {filteredList.tanggalMasuk}</td>
+                <td>
+                  {filteredList.jumlah} {filteredList.satuan}
+                </td>
+              </tr>
+            ))}
+        </table>
+      </section>
     </div>
   );
 }
